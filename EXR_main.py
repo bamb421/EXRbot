0c3055208572caac35b275bcd146a61b2f18eefa
bamb421 <84636375+bamb421@users.noreply.github.com>
2026-03-09 17:23:44 +1300

    aaaaaa

--- EXR_main.py
+++ EXR_main.py
@@ -1,23 +1,39 @@
 # this bot is designed to keep xare in check because he is A FAT ANNOYING NOOB
-# note: plz disable pings in your server because this bot wont prevent them, this is only to punish xare if he mass pings
-# also this is like my third discord bot so the code is kinda buns
+# note: plz disable pings in your server and this wont prevent them, this is only to punish xare if he mass pings
+
 import os
+import asyncio
 import discord
 import re
+import datetime
+import json
 from flask import Flask
 from threading import Thread
+from discord.ext import tasks
 from datetime import timedelta
 from discord import Embed
 
+with open("/etc/secrets/keeds.json") as f:
+    config = json.load(f)
+
 PORT = int(os.environ.get("PORT", 5000))  # port
 discordTOKEN = os.environ.get("DISCORD_TOKEN")
-timeoutLength = 300 # about an hour
+timeoutLength = 300
 devId = int(os.environ.get("devId")) # accidentally put the wrong user id oops
-kidsToKeepInCheck = [1226345314194161726, 574701344775077899] #int(os.environ.get("kidToKeepInCheck"))
+kidsToKeepInCheck = set(config["kidsToKeepInCheck"])
 timesThisKidHasPinged = 0 # explanatory
 pingMin = int(os.environ.get("pingMin")) # amount of pings that can happen before timeout
 chances = pingMin + 1 # yeah
-#amountOfPingsBeforeKick = 10 # how many pings need to have happpened before we kick xare (unused because yeah)
+aaaa = 0
+mineemum = 2 # idk anymore
+
+intents = discord.Intents.default()
+intents.message_content = True # required to read messages
+intents.members = True # required to check pings in a message
+
+start_time = datetime.datetime.now()
+
+client = discord.Client(intents=intents)
 
 app = Flask('')
 
@@ -30,11 +46,20 @@ def run_web():
 
 Thread(target=run_web).start()
 
-intents = discord.Intents.default()
-intents.message_content = True # required to read messages
-intents.members = True # required to check pings in a message
 
-client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print(f'logged in as {client.user}')
+    update_status.start()
+
+@tasks.loop(seconds=10)
+async def update_status():
+    delta = datetime.datetime.now() - start_time
+    uptime_str = format_uptime(delta)
+    await client.change_presence(
+        activity=discord.Game(name=f"Uptime: {uptime_str}"),
+        status=discord.Status.online
+    )
 
 async def send_embed(channel, author, title, description, fields=None, color=0xFF5733):
 
@@ -46,68 +71,26 @@ async def send_embed(channel, author, title, description, fields=None, color=0xF
 
     await channel.send(embed=embed)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+def format_uptime(delta):
+    total_minutes = int(delta.total_seconds() // 60)
+    hours, minutes = divmod(total_minutes, 60)
+    return f"{hours}h {minutes}m"
 
-@client.event
-async def on_message(message):
-    global timesThisKidHasPinged
-    global timeoutLength
-    global pingMin
-    global chances
+async def keeed(): 
+    global aaaa 
+    aaaa += 1  
+    await asyncio.sleep(5) 
+    aaaa -= 0
 
-    guild = message.guild
+async def timeout(guild, message):
+            global timesThisKidHasPinged
+            global chances
+            global aaaa
+            global pingMin
 
-    if message.author == client.user:
-        return
-    
-    if (len(message.mentions) > 0 or message.mention_everyone) and message.author.id in kidsToKeepInCheck:
-        #print(f"{message.author.id} and {kidToKeepInCheck}")
-
-        timesThisKidHasPinged += 1
-
-        user_pings = re.findall(r"<@!?\d+>", message.content)
-        role_pings = re.findall(r"<@&\d+>", message.content)
-        channel_pings = re.findall(r"<#\d+>", message.content)
-        everyone_pings = re.findall(r"@everyone|@here", message.content)
-
-        totalPings = (
-            len(user_pings) +
-            len(role_pings) +
-            len(channel_pings) +
-            len(everyone_pings)
-        )
-
-        if message.mention_everyone: # lololololololo
-            timesThisKidHasPinged = 100 
-            totalPings = 100
-        #print("pings:", totalPings)
-
-        await message.delete()  
-
-        if timesThisKidHasPinged <= pingMin and totalPings <= pingMin:
-            chances -= 1
-            #await message.channel.send(f"i detected a ping from you {message.author.mention}\nif you keep it up then you will be timed out for {timeoutLength} seconds\nyou have {chances} chances left\nnote: pinging more people than your remaining chances will result in a timeout!")
-
-            fields = [
-                ("warning", f"if you keep it up, you'll be timed out for **{timeoutLength} seconds**", False),
-                ("chances left", f"you have **{chances}** chances remaining.\nnote: pinging more people than your remaining chances or using `@everyone` will result in an instant timeout!", False)
-            ]
-            
-            await send_embed(
-                channel=message.channel,
-                author=message.author,
-                title="NOTIFICATION",
-                description=f"i detected a ping from you, {message.author.mention}!",
-                fields=fields,
-                color=0xFF5733
-            )
-        else:
-            # reset da stuff we changed
             timesThisKidHasPinged = 0
             chances = pingMin + 1
-
+            aaaa = 0
             try:
                 TimeoutReason = f"{message.author.mention} has pinged over the ping limit which is {pingMin}, the user has been sent to timeout for {timeoutLength} seconds."
 
@@ -156,16 +139,77 @@ async def on_message(message):
                     fields=fields,
                     color=0xFF3333
                 )
+            await message.delete()  
+async def warn(message):
+            global chances
+
+            chances -= 1
+            #await message.channel.send(f"i detected a ping from you {message.author.mention}\nif you keep it up then you will be timed out for {timeoutLength} seconds\nyou have {chances} chances left\nnote: pinging more people than your remaining chances will result in a timeout!")
+
+            fields = [
+                ("warning", f"if you keep it up, you'll be timed out for **{timeoutLength} seconds**", False),
+                ("chances left", f"you have **{chances}** chances remaining.\nnote: pinging more people than your remaining chances or using `@everyone` will result in an instant timeout!", False)
+            ]
+            
+            await send_embed(
+                channel=message.channel,
+                author=message.author,
+                title="NOTIFICATION",
+                description=f"i detected a ping from you, {message.author.mention}!",
+                fields=fields,
+                color=0xFF5733
+            )
+            await message.delete()  
+
+@client.event
+async def on_message(message):
+    global timesThisKidHasPinged
+    global timeoutLength
+    global pingMin
+    global chances
+    global aaaa
+    global mineemum
+
+    guild = message.guild
+
+    if message.author == client.user:
+        return
+    
+    if (len(message.mentions) > 0 or message.mention_everyone) and message.author.id in kidsToKeepInCheck:
+        #print(f"{message.author.id} and {kidToKeepInCheck}")
+        asyncio.create_task(keeed())
+
+        if message.mention_everyone: # lololololololo
+            await timeout(guild, message)
+            
+        #print("pings:", totalPings)
+
+        user_pings = re.findall(r"<@!?\d+>", message.content)
+        role_pings = re.findall(r"<@&\d+>", message.content)
+        channel_pings = re.findall(r"<#\d+>", message.content)
+        everyone_pings = re.findall(r"@everyone|@here", message.content)
+
+        totalPings = (
+            len(user_pings) +
+            len(role_pings) +
+            len(channel_pings) +
+            len(everyone_pings)
+        )
+
+        print(f"AAAA: {aaaa}, MINEEMUM: {mineemum}, totalPings: {totalPings}, chances: {chances}")
+
+        if aaaa <= mineemum and totalPings <= mineemum:
+            return
+
+        if timesThisKidHasPinged <= pingMin and totalPings <= pingMin:
+            await warn(chances, message)
+        elif timesThisKidHasPinged >= pingMin and totalPings >= pingMin:
+            await timeout(guild, message)
+
                 #await message.channel.send(f"AN ERROR HAS OCCURRED: {e}\n<@{devId}>")
       
     #if message.content.startswith('$hello') and :
     #    print("USER ID FOUND IN kidsToKeepInCheck ", message.author.id)
     #    await message.channel.send('Hello!')
 
-
 client.run(discordTOKEN)
-
-
-
-
-
